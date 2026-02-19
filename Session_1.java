7d00b26c89f9f4cbeedebff1ae436eb389bef865
Syed Rizwan <syedbbictech@gmail.com>
2026-02-19 16:13:07 +0530

    Add files via upload

---
+++ Session_1.java
@@ -0,0 +1,33 @@
+package CSE_1;
+
+public class Session_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*System.out.println("Hello Java");
+		
+		String name = "Syed";
+		int age=28;
+		System.out.print("my name is " + name + " i am " +age+" years old");*/
+		
+		//valid
+		int my_value=10;
+		int myvalue1 =20;
+		int $myvalue=30;
+		int _myval=40;
+		System.out.println(my_value);
+		System.out.println(myvalue1);
+		System.out.println($myvalue);
+		System.out.println(_myval);
+		
+		//invalid
+		/*int 1number=100;
+		System.out.println(1number);*/
+		
+		
+		
+		
+	}
+
+}
