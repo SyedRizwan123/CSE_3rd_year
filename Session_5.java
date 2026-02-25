18f052e494ef4b818feb7d4696cf8156e60eca66
Syed Rizwan <syedbbictech@gmail.com>
2026-02-25 12:52:57 +0530

    Add files via upload

---
+++ Session_5.java
@@ -0,0 +1,39 @@
+package CSE_1;
+
+/*public class Session_5 {
+	static void myMethod() {
+	    System.out.println("I just got executed!");
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myMethod();
+	}
+
+}*/
+
+/*public class Session_5 {
+	static void myMethod(String fname) {
+	    System.out.println(fname + " Syed");
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myMethod("Rizwan");
+		myMethod("John");
+	}
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
